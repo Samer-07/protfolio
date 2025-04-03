362f18906b22a12a7814802ba51683f83f074a24
Samer Elhadey <samerabuhachich@gmail.com>
2025-04-03 20:10:54 +0200

    New project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -10,6 +10,12 @@ const projects = [
     link: "https://github.com/Samer-07/VardagStil-project"
   },
   {
+    title: "Freaky-Fashion",
+    description: " Är en dynamisk e-handelsplattform byggd med React (frontend) och en backend kopplad till en databas. Projektet möjliggör dynamisk visning av produkter via props, samt en sida för att lägga till nya produkter. Det inkluderar även en sökfunktion som filtrerar produkter i realtid, vilket gör det enkelt för användare att hitta det de söker.",
+    image: "./images/logo.png",
+    link: "https://github.com/Samer-07/Freaky-Fashion.git"
+  },
+  {
     title: "movie-list-app",
     description: "Detta är en React-applikation som visar några av de färdigheter jag har lärt mig under min utbildning. Genom denna webbplats har jag utvecklat en djupare förståelse för att bygga användargränssnitt och hantera API-förfrågningar, samt implementera funktioner som genre-filter och visning av filmer baserat på användarval. Jag har också implementerat möjligheten att spara favoritfilmer för användare.",
     image: "./images/OIP.jpeg",
